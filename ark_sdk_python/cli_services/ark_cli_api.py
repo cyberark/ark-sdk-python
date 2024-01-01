d4555fe7fa772b5712b199f8cd87d0958c731211
ofir iluz <ofir.iluz@cyberark.com>
2024-01-01 11:28:41 +0200

    ARK-600: Minor fixes to missing API's (#4)

--- ark_sdk_python/cli_services/ark_cli_api.py
+++ ark_sdk_python/cli_services/ark_cli_api.py
@@ -5,6 +5,18 @@ from ark_sdk_python.ark_api import ArkAPI
 
 class ArkCLIAPI(ArkAPI):
     @property
+    def dpa_policies_vm_editor(self) -> "ArkDPAVMPoliciesEditorService":
+        """
+        VM policy editor CLI service
+
+        Returns:
+            ArkDPAVMPoliciesEditorService: _description_
+        """
+        from ark_sdk_python.cli_services.dpa.vm import ArkDPAVMPoliciesEditorService
+
+        return ArkDPAVMPoliciesEditorService(self.authenticator('isp'), profile=self.profile)
+
+    @property
     def dpa_policies_db_editor(self) -> "ArkDPADBPoliciesEditorService":
         """
         DB policy editor CLI service
