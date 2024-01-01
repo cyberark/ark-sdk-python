57b2d053b5a6ddee7167a15eea95002e5d2b5c5f
Ofir Iluz <ofir.iluz@cyberark.com>
2024-01-01 11:27:27 +0200

    ARK-600: Minor fixes to missing API's

--- ark_sdk_python/services/dpa/ark_dpa_api.py
+++ ark_sdk_python/services/dpa/ark_dpa_api.py
@@ -3,6 +3,7 @@ from ark_sdk_python.services.dpa.certificates import ArkDPACertificatesService
 from ark_sdk_python.services.dpa.db import ArkDPADBService
 from ark_sdk_python.services.dpa.k8s import ArkDPAK8SService
 from ark_sdk_python.services.dpa.policies.db import ArkDPADBPoliciesService
+from ark_sdk_python.services.dpa.policies.vm import ArkDPAVMPoliciesService
 from ark_sdk_python.services.dpa.secrets.db import ArkDPADBSecretsService
 from ark_sdk_python.services.dpa.sso import ArkDPASSOService
 from ark_sdk_python.services.dpa.workspaces.db import ArkDPADBWorkspaceService
@@ -11,6 +12,7 @@ from ark_sdk_python.services.dpa.workspaces.db import ArkDPADBWorkspaceService
 class ArkDPAAPI:
     def __init__(self, isp_auth: ArkISPAuth) -> None:
         self.__db_workspace_service = ArkDPADBWorkspaceService(isp_auth)
+        self.__vm_policies_service = ArkDPAVMPoliciesService(isp_auth)
         self.__db_policies_service = ArkDPADBPoliciesService(isp_auth)
         self.__db_secrets_service = ArkDPADBSecretsService(isp_auth)
         self.__sso_service = ArkDPASSOService(isp_auth)
@@ -27,6 +29,16 @@ class ArkDPAAPI:
             ArkDPADBWorkspaceService: _description_
         """
         return self.__db_workspace_service
+
+    @property
+    def policies_vm(self) -> ArkDPAVMPoliciesService:
+        """
+        Getter for the VM policies service.
+
+        Returns:
+            ArkDPAVMPoliciesService: _description_
+        """
+        return self.__vm_policies_service
 
     @property
     def policies_db(self) -> ArkDPADBPoliciesService:
