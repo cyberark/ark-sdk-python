bf2753ffba81070094b91693083be9e99252f974
Ofir Iluz <ofir.iluz@cyberark.com>
2025-08-13 12:09:34 +0300

    Support UAP VM, DB, and Cloud Consoles, also support SIA Settings

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     with open(path.join(path.abspath(path.dirname(__file__)), 'pyproject.toml'), 'r', encoding='utf-8') as f:
         data = f.read()
         version = re.findall('version = \".*\..*\..*\"', data)[0].split('version = ')[1].strip().replace('\"', '')
-    with open(path.join(path.abspath(path.dirname(__file__)), 'ark_sdk_python', 'ark.py'), 'r+',
+    with open(path.join(path.abspath(path.dirname(__file__)), 'ark_sdk_python', 'common', 'ark_version.py'), 'r+',
             encoding='utf-8') as f:
         ark_data = f.read()
         ark_data = re.sub('__version__ =.*', f'__version__ = \'{version}\'', ark_data)
