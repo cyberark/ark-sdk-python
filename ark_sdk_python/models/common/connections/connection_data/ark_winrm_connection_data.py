db8353f362d5bb2228286360a1b4cb94fd5d72d7
ofir iluz <ofir.iluz@cyberark.com>
2025-11-16 10:37:37 +0200

    ARK-000: Fix hardcoded ignore cert validation to be parameterized with validate as default (#50)

--- ark_sdk_python/models/common/connections/connection_data/ark_winrm_connection_data.py
+++ ark_sdk_python/models/common/connections/connection_data/ark_winrm_connection_data.py
@@ -7,3 +7,4 @@ from ark_sdk_python.models import ArkModel
 
 class ArkWinRMConnectionData(ArkModel):
     certificate: Optional[str] = Field(description="Certificate to use for connection transport", default=None)
+    validate_certificate: bool = Field(description="Whether to validate the SSL certificate", default=True)
